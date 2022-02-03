0a5cf76a266cff14ebaf0fc4e872f0c6759443ee
Francesco Maestrelli <framae26@gmail.com>
2022-02-03 15:04:56 +0100

    snack1

---
+++ Snack1/main.js
@@ -0,0 +1,22 @@
+/*Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal */
+
+const myArray = [
+
+
+    bici1 = {
+        nome: 'thunder',
+        peso: '2kg'
+    },
+
+    bici2 = {
+        nome: 'bolt',
+        peso: '1,8kg'
+    },
+
+    bici3 = {
+        nome: 'lightening',
+        peso: '1,7kg'
+    };
+];
