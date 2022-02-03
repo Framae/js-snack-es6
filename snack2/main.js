b889a1f323b43d7ec04fcee682c31fe303308a70
Francesco Maestrelli <framae26@gmail.com>
2022-02-03 16:42:03 +0100

    random numbers

--- snack2/main.js
+++ snack2/main.js
@@ -1,5 +1,6 @@
 // Snack2 
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 
 
 const myArray = [
@@ -26,8 +27,16 @@ console.log(myArray);
 
 
 
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà:
-// Punti fatti e falli subiti.
+
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+
+let varPunti = Math.floor(Math.random()*10) + 1;
+console.log(varPunti);
+
+let varFalliSubiti = Math.floor(Math.random()*10) + 1;
+console.log(varFalliSubiti);
+
+
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. 
 
